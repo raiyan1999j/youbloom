581b33d98a2ade92b7d6171fcf40410105033cc6
Raiyan <boomboomraiyan@gmail.com>
2025-09-24 22:14:40 +0600

    mainpage - pagination & search feature

--- src/pages/main/items.tsx
+++ src/pages/main/items.tsx
@@ -3,18 +3,14 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import ImageValidation from "./imgvalidation";
 import { BiSolidCategory } from "react-icons/bi";
+import Loader from "../../components/ui/loader";
+import Error from "../../components/ui/error";
 
 type DataLimitaion = {
     currentPage: number,
     start: number,
     limit: number,
     totalPage: number | null
-}
-
-type PaginationType = {
-    firstPhase: number[],
-    middlePhase: number[],
-    lastPhase: number[]
 }
 
 type UserData = {
@@ -38,16 +34,12 @@ export default function Items(){
         totalPage:null
     });
 
-    const [pagination,setPagination] = useState<PaginationType>({
-        firstPhase:[],
-        middlePhase:[],
-        lastPhase:[]
-    });
+    const [searchInput,setSearchInput] = useState<string>("");
 
     const {isLoading,isError,data} = useQuery<UserData>({
-        queryKey:["userData",dataLimitation.limit],
+        queryKey:["userData",dataLimitation.limit,dataLimitation.start,searchInput],
         queryFn:async ()=>{
-            const getData = await axios(`https://jsonplaceholder.typicode.com/photos?_page=1&_start=${dataLimitation.start}&_limit=${dataLimitation.limit}`);
+            const getData = await axios(`https://jsonplaceholder.typicode.com/photos?_start=${dataLimitation.start}&_limit=${dataLimitation.limit}&q=${searchInput}`);
             let wrap;
 
             const totalData= Number(getData.headers["x-total-count"] || 0);
@@ -60,24 +52,51 @@ export default function Items(){
         }
     });
 
-    
+    const pagination=(totalPages:number,currentPage:number)=>{
+        const pages = [];
+
+        for(let i=1; i<= Math.min(4,totalPages);i++){
+            pages.push(i)
+        }
+
+        let start = Math.max(currentPage-1,5);
+        let end = Math.min(currentPage+2, totalPages-4);
+
+        if(start > 5) pages.push("...");
+        for(let i = start; i <= end; i++){
+            pages.push(i)
+        }
+
+        if(end < totalPages - 4) pages.push("...");
+
+        for(let i = Math.max(totalPages - 3,5); i<= totalPages; i++){
+            if(i > 4) pages.push(i)
+        }
+
+        return pages;
+    }
+
+    const updateCurrentPage=(currentPage:number)=>{
+        const start = (currentPage * dataLimitation.limit) - dataLimitation.limit;
+
+        setLimitation(prev=>({...prev,currentPage,start}));
+    }
 
     useEffect(()=>{
         if(data){
             const totalPage = Math.ceil((data.totalData)/dataLimitation.limit);
-            const division = Math.ceil(totalPage/3);
-            const firstPhase= [...Array(4)].map((_,index)=>index+1);
-            const middlePhase=[...Array(4)].map((_,index)=>index+division);
-            const lastPhase = [...Array(4)].map((_,index)=>totalPage-index).reverse();
             
-            setPagination({firstPhase,middlePhase,lastPhase});
+            setLimitation(prev=>({...prev,totalPage}));
         }
     },[data]);
-    return(
+
+    const generatePages = pagination(dataLimitation.totalPage??0,dataLimitation.currentPage);
+
+    return( 
         <>
         <section className="mt-20">
             <div className="w-[50%] mx-auto h-10">
-                <input type="text" name="search" placeholder="Search" className="nunito text-black/80 font-bold h-full w-full bg-gray-300/40 rounded-xl px-5 nunito placeholder:text-black/20 focus:outline-none focus:border-gray-400 focus:border" />
+                <input type="text" name="search" value={searchInput??""} placeholder="Search" className="nunito text-black/80 font-bold h-full w-full bg-gray-300/40 rounded-xl px-5 nunito placeholder:text-black/20 focus:outline-none focus:border-gray-400 focus:border" onChange={(event)=>{setSearchInput(event.target.value)}}/>
             </div>
         </section>
 
@@ -98,7 +117,7 @@ export default function Items(){
                     <select value={dataLimitation.limit} className="border-b border-b-black/80" onChange={(event)=>{setLimitation(prev=>({...prev,limit:parseInt(event.target.value)}))}}>
                         {
                             [...Array(10)].map((_,index)=>{
-                                return <option value={(index + 1)*10}>{(index + 1) * 10}</option>
+                                return <option value={(index + 1)*10} key={index}>{(index + 1) * 10}</option>
                             })
                         }
                     </select>
@@ -113,9 +132,19 @@ export default function Items(){
         </section>
 
         <section className="px-20 mt-10">
-            <div className="grid grid-cols-3 gap-x-5 gap-y-[60px] px-10 py-10 h-[500px] overflow-y-scroll sideScrollbar">
+            {
+                isLoading?
+                <div className="flex justify-center items-center w-full h-[500px]">
+                    <Loader/>
+                </div>:
+                isError?
+                <div>
+                    <Error/>
+                </div>:
+                data?
+                <div className="grid grid-cols-3 gap-x-5 gap-y-[60px] px-10 py-10 h-[500px] overflow-y-scroll sideScrollbar">
                 {
-                    data?.data.map((items,index)=>{
+                    data.data.map((items,index)=>{
                         return <div className="shadow-black/20 shadow-sm rounded-xl space-y-5 transition-all duration-150 ease-linear hover:shadow-black/80" key={index}>
                             <div className="w-full h-[250px] border-b border-black/20 rounded-xl flex justify-center items-center">
                                 <ImageValidation imgUrl={items.url}/>
@@ -147,48 +176,25 @@ export default function Items(){
                         </div>
                     })
                 }
-            </div>
+            </div>:
+            null
+            }
+            
         </section>
 
-        <section className="px-20 mt-20">
-            <div className="flex flex-row justify-end w-full">
-                <div className="flex flex-row gap-x-5">
-                    {
-                        pagination.firstPhase.map((items,index)=>{
-                            return <button className={`nunito px-2.5 py-1.5 flex justify-center items-center border border-black/20 rounded-lg transition-all duration-150 ease-linear hover:bg-black/20 hover:text-white hover:cursor-pointer ${dataLimitation.currentPage === items?"bg-black/20 text-white":"bg-transparent"}`} key={index} onClick={()=>{setLimitation(prev=>({...prev,currentPage:items}))}}>
-                                {items}
-                            </button>
-                        })
-                    }
-                </div>
-                <div className="flex flex-row gap-x-2.5 nunito text-black/80 items-end mx-2.5">
-                    {
-                        [...Array(6)].map((_,index)=>{
-                            return <span key={index}>.</span>
-                        })
-                    }
-                </div>
-                <div className="flex flex-row gap-x-5">
-                    {pagination.middlePhase.map((items,index)=>{
-                        return <button className={`nunito px-2.5 py-1.5 flex justify-center items-center border border-black/20 rounded-lg transition-all duration-150 ease-linear hover:bg-black/20 hover:text-white hover:cursor-pointer ${dataLimitation.currentPage === items?"bg-black/20 text-white":"bg-transparent"}`} key={index} onClick={()=>{setLimitation(prev=>({...prev,currentPage:items}))}}>
-                            {items}
+        <section className="px-20">
+            <div className="flex flex-row justify-end items-end gap-x-5 w-full px-10">
+                {
+                    generatePages.map((items,index)=>{
+                        return items === "..."?
+                        <span className="nunito" key={index}>
+                            ...
+                        </span>:
+                        <button className={`nunito py-1.5 px-2.5 rounded-lg border border-black/20 ${dataLimitation.currentPage === items?"bg-black/20 text-white":"bg-transparent text-black/80"} transition-all duration-150 ease-linear hover:cursor-pointer hover:bg-black/20 hover:text-white`} key={index} onClick={()=>{updateCurrentPage(Number(items))}}>
+                        {items}
                         </button>
-                    })}
-                </div>
-                <div className="flex flex-row gap-x-2.5 nunito text-black/80 items-end mx-2.5">
-                    {
-                        [...Array(6)].map((_,index)=>{
-                            return <span key={index}>.</span>
-                        })
-                    }
-                </div>
-                <div className="flex flex-row gap-x-5">
-                    {pagination.lastPhase.map((items,index)=>{
-                        return <button className={`nunito px-2.5 py-1.5 flex justify-center items-center border border-black/20 rounded-lg transition-all duration-150 ease-linear hover:bg-black/20 hover:text-white hover:cursor-pointer ${dataLimitation.currentPage === items?"bg-black/20 text-white":"bg-transparent"}`} key={index} onClick={()=>{setLimitation(prev=>({...prev,currentPage:items}))}}>
-                            {items}
-                        </button>
-                    })}
-                </div>
+                    })
+                }
             </div>
         </section>
         </>
