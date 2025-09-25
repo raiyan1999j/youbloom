2e4143c624f1adb06f66aae819e4bffa6f6a6091
Raiyan <boomboomraiyan@gmail.com>
2025-09-25 12:19:10 +0600

    error ui

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,29 +1,38 @@
+import { useContext } from "react"
 import { FaHome } from "react-icons/fa"
 import { MdExitToApp } from "react-icons/md"
-import { TbFileDescription } from "react-icons/tb"
-import { NavLink } from "react-router-dom"
+import { NavLink, useLocation } from "react-router-dom"
+import { InfoContainer } from "../context/contextprovider"
 
 type PagesType = {
     link:string,
+    title:string,
     icon:React.ReactNode
 }
 
 const pages:PagesType[] = [
     {
         link:"/",
+        title:"main",
         icon:<FaHome />
     },
-    {
-        link:"/details",
-        icon:<TbFileDescription />
-    },
-    {
-        link:"#",
-        icon:<MdExitToApp />
-    }
 ]
 
 export default function Sidebar(){
+    const context = useContext(InfoContainer);
+
+    if(!context) throw new Error("context error");
+
+    const {setUserLogin} = context;
+    const location = useLocation();
+
+    const logoutUser=()=>{
+        setUserLogin(false);
+
+        localStorage.setItem("userLogin","false");
+    }
+
+    console.log(location)
     return(
         <>
         <div className="flex flex-col justify-center gap-y-10 fixed top-[25%] right-[2%] h-1/2 px-2.5 before:absolute before:content-'' before:h-full before:w-[2px] before:bg-black before:right-[50%]">
@@ -33,6 +42,13 @@ export default function Sidebar(){
                         {items.icon}
                     </NavLink>
                 })
+            }
+
+            {
+                location.pathname !== "/login" &&
+                <button className={`h-10 w-10 rounded-full border border-black flex justify-center items-center z-40 bg-white transition-all duration-150 ease-linear hover:bg-black hover:text-white`} onClick={logoutUser}>
+                    <MdExitToApp />
+                </button>
             }
         </div>
         </>
--- src/components/ui/error.tsx
+++ src/components/ui/error.tsx
@@ -2,6 +2,27 @@
 export default function Error(){
     return(
         <>
+        <section className="px-20 mt-20">
+            <div className="w-[50%] mx-auto flex flex-col items-center shadow-sm shadow-black/20 py-10 rounded-xl">
+                <div>
+                    <h1 className="caprasiom text-2xl text-rose-500! capitalize">
+                        Something went wrong
+                    </h1>
+                </div>
+
+                <div className="mt-10">
+                    <p className="nunito text-base tracking-widest">
+                        Please try again
+                    </p>
+                </div>
+
+                <div className="mt-10">
+                    <button className="px-3.5 py-2 bg-sky-500 rounded-full text-white nunito font-bold capitalize transition-all duration-150 ease-linear hover:bg-sky-800" onClick={()=>window.location.reload()}>
+                        reload
+                    </button>
+                </div>
+            </div>
+        </section>
         </>
     )
 }
